c7bdf75aecd591f74f000eeda0272ca70868a616
CeliaDiaz17 <64320930+CeliaDiaz17@users.noreply.github.com>
2020-11-10 00:57:41 +0100

    Tested Heap implementation

--- Heap.py
+++ Heap.py
@@ -1,46 +1,51 @@
-class Heap(object):
+def heapify(arr, n, i):
+    largest = i
+    leftChild = 2 * i + 1
+    rightChild   = 2 * i + 2 
+    
+    if leftChild < n and arr[i] < arr[leftChild]:
+        largest = leftChild
+    
+    if rightChild < n and arr[largest] < arr[rightChild]:
+        largest = rightChild
+    
+    if largest != i:
+        arr[i],arr[largest] = arr[largest],arr[i]
+        heapify(arr, n, largest)
 
-    def heapify(self,arr, size, i):  #Method to heapify the tree
-        #set i as largest
-        largest = i;
-        #leftChild = 2i +1
-        leftChild = 2*i +1
-        #rightChild = 2i +2
-        rightChild = 2*i  +2
+def insert(array, newNode):
+    size = len(array)
+    if size == 0:
+        array.append(newNode)
+    else:
+        array.append(newNode);
+        for i in range((size//2)-1, -1, -1):
+            heapify(array, size, i)
+
+
+def deleteNode(array, id):
+    size = len(array)
+    i = 0
+    for i in range(0, size):
+        if id == array[i]:
+            break
         
-        if leftChild < size and arr[i] < arr[leftChild]:
-            #set leftChildIndex as largest
-            largest = leftChild
-        if rightChild < size and arr[i] < arr[rightChild]:
-            #set rightChildIndex as largest
-            largest = rightChild
+    array[i], array[size-1] = array[size-1], array[i]
 
-       #swap array[i] and array[largest] 
-        if largest != i:
-            arr[i], arr[largest] = arr[largest], arr[i]
-            heapify(arr, size, largest)
+    array.remove(size-1)
+    
+    for i in range((len(array)//2)-1, -1, -1):
+        heapify(array, len(array), i)
+    
+arr = []
 
-    def insertNode(self,arr, newNode):
-        size = len(arr)
-        #If there is no node,
-        if size == 0:
-            #dabuti metes el nodo
-            arr.append(newNode)
-        #else si ya hay algo dentro
-        else:
-            #insert el newNode al final 
-            for i in range((size//2)-1,-1,-1):
-                heapify(arr,size,i)
+insert(arr, 3)
+insert(arr, 4)
+insert(arr, 9)
+insert(arr, 5)
+insert(arr, 2)
 
-    def deleteNode(self,arr, idNode):
-        size = len(arr)
-        i = 0
-        #if nodetoDelete is the leafNode
-        for i in range (0, size):
-            if idNode == arr[i]:
-                break
-         #swap nodetoDelete with the lastLeafNode   
-        arr[i], arr[size-1] = arr[size-1], arr[i]
-        arr.remove(size-1) #remove the nodeTodelete
-        for i in range((len(arr)//2)-1,-1,-1):
-            heapify(arr, len(arr),i) 
+print ("Max-Heap array: " + str(arr))
+
+deleteNode(arr, 4)
+print("After deleting an element: " + str(arr))
